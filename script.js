a56ad02be36e5a5e8777d1f2101378614c733898
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-03 21:37:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMPDFile() {
+  return "https://ais-s.ais-vidnt.com/ais/play/origin/live/eds/S0001/DASH/S0001.mpd";
+}
+
+function getClearKey() {
+  return "3fd52d1e9ba4b180aec12c9295b62c0f";
+}
+
+function getClearKeyId() {
+  return "bb274e33da0c709e62c55d0cba911889";
+}
